cb9bfa5a829430214802adc763c6831199ccafd3
ruthergit <121865370+ruthergit@users.noreply.github.com>
2024-05-23 00:03:58 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let buttonAct = document.getElementById("submitBtn");
+
+    buttonAct.addEventListener("click", (e) => {
+        window.location.href = 'frontpage.html';
+        alert("fsdfas")
+    });
+});

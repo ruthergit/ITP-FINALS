53e844961b108944af35ab547daec76f1c9fd187
ruthergit <121865370+ruthergit@users.noreply.github.com>
2024-05-30 14:42:18 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    let buttonAct = document.getElementById("submitBtn");
-
-    buttonAct.addEventListener("click", (e) => {
-        window.location.href = 'frontpage.html';
-        alert("fsdfas")
-    });
-});
